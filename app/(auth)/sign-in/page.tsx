bce38c126352af8f8480b282b441aca96dd6e0d7
Andres Argote <andresargote2019@gmail.com>
2024-04-13 15:32:27 +0200

    feat: 🎸 Added initial styles
    Added initial css variables in the globals css file

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Iniciar sesión',
+  description: 'Inicia sesión en tu cuenta de MiDoctor',
+};
+
+export default function Signin() {
+  return <p>Signin</p>;
+}
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Registrarse',
+  description: 'Crea una cuenta en MiDoctor',
+};
+
+export default function Signup() {
+  return <p>Signup</p>;
+}
