423494900342eee23ecd122294d5d53ab0d48738
Andres Argote <andresargote2019@gmail.com>
2024-06-10 20:20:47 +0200

    feat: 🎸 Added changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <p className="text-3xl font-bold underline">Telemedicine super app</p>
+      <p className="text-3xl font-bold underline text-primary-500">
+        Telemedicine super app
+      </p>
     </main>
   );
 }
