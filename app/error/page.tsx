ace698c004fb250b124a4cd7e6cc67e42160149f
Andres Argote <andresargote2019@gmail.com>
2024-06-24 20:46:42 +0200

    feat: 🎸 Added simple error page
    Agregaga pagina de Error custom, la idea es que dependiendo de un
    queryParam podamos mostrar uno u otro mensaje de error

---
+++ app/error/page.tsx
@@ -0,0 +1,8 @@
+export default function Error() {
+  return (
+    <div>
+      <h1>¡Ups! Algo salió mal.</h1>
+      <p>Si el error persiste, por favor contacta al soporte.</p>
+    </div>
+  );
+}
