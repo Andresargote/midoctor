ec5b054b032690f7f699dd23e38097f68566dca2
Andres Argote <andresargote2019@gmail.com>
2024-08-31 18:12:43 +0200

    feat: 🎸 Added new select component
    Agregado nuevo select custom componente y aplicado en el componente
    Availability y ServicesList

--- app/app/mis-servicios/page.tsx
+++ app/app/mis-servicios/page.tsx
@@ -1,47 +1,47 @@
-import { createClient } from "@/app/lib/utils/supabase/server";
-import { ServicesList } from "@/app/ui/components/ServicesList";
-import Link from "next/link";
-import { Suspense } from "react";
+import { createClient } from '@/app/lib/utils/supabase/server';
+import { ServicesList } from '@/app/ui/components/ServicesList';
+import Link from 'next/link';
+import { Suspense } from 'react';
 
 export default async function MisServicios() {
-	const supabase = createClient();
+  const supabase = createClient();
 
-	const { data } = await supabase.auth.getUser();
+  const { data } = await supabase.auth.getUser();
 
-	const services = await supabase
-		.from("services")
-		.select("*")
-		.eq("owner_id", data?.user?.id);
+  const services = await supabase
+    .from('services')
+    .select('*')
+    .eq('owner_id', data?.user?.id);
 
-	return (
-		<div className="px-4 py-6 mx-auto">
-			<div className="container mx-auto lg:pl-72">
-				<header className="flex flex-col justify-between w-full gap-6 mb-12 md:flex-row">
-					<div>
-						<h1 className="mb-2 text-3xl font-semibold text-neutral-900">
-							Mis Servicios
-						</h1>
-						<p className="font-light leading-relaxed text-neutral-800">
-							Aquí puedes ver y editar la información de tus servicios.
-						</p>
-					</div>
-					<div>
-						<Link
-							className="flex flex-col justify-center px-4 font-semibold transition duration-300 rounded-full w-fit bg-primary-500 hover:bg-primary-600 focused-btn min-h-14 text-f-white"
-							href="mis-servicios/?action=new"
-						>
-							Agregar servicio
-						</Link>
-					</div>
-				</header>
+  return (
+    <div className='px-4 py-6 mx-auto'>
+      <div className='container mx-auto lg:pl-72'>
+        <header className='flex flex-col justify-between w-full gap-6 mb-12 md:flex-row'>
+          <div>
+            <h1 className='mb-2 text-3xl font-semibold text-neutral-900'>
+              Mis Servicios
+            </h1>
+            <p className='font-light leading-relaxed text-neutral-800'>
+              Aquí puedes ver y editar la información de tus servicios.
+            </p>
+          </div>
+          <div>
+            <Link
+              className='flex flex-col justify-center px-4 font-semibold transition duration-300 rounded-full w-fit bg-primary-500 hover:bg-primary-600 focused-btn min-h-14 text-f-white'
+              href='mis-servicios/?action=new'
+            >
+              Agregar servicio
+            </Link>
+          </div>
+        </header>
 
-				<Suspense fallback={<div>Cargando...</div>}>
-					<ServicesList
-						userId={data?.user?.id ?? ""}
-						servicesData={services.data ?? []}
-					/>
-				</Suspense>
-			</div>
-		</div>
-	);
+        <Suspense fallback={<div>Cargando...</div>}>
+          <ServicesList
+            userId={data?.user?.id ?? ''}
+            servicesData={services.data ?? []}
+          />
+        </Suspense>
+      </div>
+    </div>
+  );
 }
--- app/ui/components/Availability/index.tsx
+++ app/ui/components/Availability/index.tsx
@@ -1,123 +1,206 @@
-"use client";
-import type { Availability as AvailabilityType, Day } from "@/app/lib/types";
-import * as Switch from "@radix-ui/react-switch";
-import { useState } from "react";
-import { PlusLg, Trash3 } from "react-bootstrap-icons";
-import Select from "../Select";
-
-/*
- - A tener en cuenta:
-	- Cuando se pasa de deshabilitado a habilitado, se debe mostrar un input para agregar franjas horarias. (hay que tener en cuenta si existe un slots ya agregado)
-	- Generar horas de inicio y fin cada 15 minutos.
-*/
+'use client';
+import type { Availability as AvailabilityType, Day, Slot } from '@/app/lib/types';
+import * as Switch from '@radix-ui/react-switch';
+import { useState } from 'react';
+import { PlusLg, Trash3 } from 'react-bootstrap-icons';
+import { generateHoursAndMinutes } from '@/app/lib/utils';
+import { SelectV2 } from '../SelectV2';
 
 export type AvailabilityProps = {
-	availability: AvailabilityType;
+  availability: AvailabilityType;
 };
 
+const START_TIMES = generateHoursAndMinutes('06:00', '23:59', 15).map((time) => ({
+  value: time,
+  label: parseInt(time.split(':')[0]) > 12 ? `${time}pm` : `${time}am`,
+}));
+const END_TIMES = generateHoursAndMinutes('06:00', '23:59', 15).map((time) => ({
+  value: time,
+  label: parseInt(time.split(':')[0]) > 12 ? `${time}pm` : `${time}am`,
+}));
+
 export function Availability({ availability }: AvailabilityProps) {
-	const [value, setValues] = useState<AvailabilityType>(availability);
+  const [value, setValues] = useState<AvailabilityType>(availability);
 
-	const handleDisableDay = (day: string) => {
-		setValues((prev) => ({
-			...prev,
-			days: prev.days.map((d) =>
-				d.day === day
-					? {
-							...d,
-							available: !d.available,
-							slots:
-								!d.available && d.slots.length === 0
-									? [{ start: "08:00", end: "18:00" }]
-									: d.slots,
-						}
-					: d,
-			),
-		}));
-	};
+  const handleDisableDay = (day: string) => {
+    setValues((prev) => ({
+      ...prev,
+      days: prev.days.map((d) =>
+        d.day === day
+          ? {
+              ...d,
+              available: !d.available,
+              slots:
+                !d.available && d.slots.length === 0
+                  ? [{ start: '08:00', end: '18:00' }]
+                  : d.slots,
+            }
+          : d
+      ),
+    }));
+  };
 
-	return (
-		<form className="flex flex-col gap-6">
-			{value.days.map((availability) => (
-				<AvailabilityRow
-					key={availability.idDay}
-					availability={availability}
-					handleDisableDay={handleDisableDay}
-				/>
-			))}
-		</form>
-	);
+  const previousStartAndEnd = (slots: Slot[]) => {
+    const lastSlot = slots[slots.length - 1];
+    const lastSlotEnd = lastSlot.end.split(':');
+
+    const start = parseInt(lastSlotEnd[0]) + 1;
+    const end = start + 1 === 24 ? 23 : start + 1;
+
+    if (start === 24) {
+      return null;
+    }
+
+    return {
+      start: `${start}:00`,
+      end: end === 23 ? '23:59' : `${end}:00`,
+    };
+  };
+
+  const handleAddSlot = (day: string) => {
+    const findDaySlots = value.days.find((d) => d.day === day)?.slots;
+
+    if (findDaySlots) {
+      const calcNewSlotHours = previousStartAndEnd(findDaySlots);
+      if (calcNewSlotHours) {
+        setValues((prev) => ({
+          ...prev,
+          days: prev.days.map((d) =>
+            d.day === day
+              ? {
+                  ...d,
+                  slots: [
+                    ...d.slots,
+                    { start: calcNewSlotHours.start, end: calcNewSlotHours.end },
+                  ],
+                }
+              : d
+          ),
+        }));
+      }
+    }
+  };
+
+  const handleRemoveSlot = (day: string, index: number) => {
+    setValues((prev) => ({
+      ...prev,
+      days: prev.days.map((d) =>
+        d.day === day ? { ...d, slots: d.slots.filter((_, i) => i !== index) } : d
+      ),
+    }));
+  };
+
+  const handleChangeSlot = (day: string, index: number, slot: Slot) => {
+    setValues((prev) => ({
+      ...prev,
+      days: prev.days.map((d) =>
+        d.day === day
+          ? {
+              ...d,
+              slots: d.slots.map((s, i) => (i === index ? slot : s)),
+            }
+          : d
+      ),
+    }));
+  };
+
+  return (
+    <form className='flex flex-col gap-6'>
+      {value.days.map((availability, index) => (
+        <AvailabilityRow
+          key={availability.idDay}
+          availability={availability}
+          handleDisableDay={handleDisableDay}
+          handleAddSlot={handleAddSlot}
+          handleRemoveSlot={handleRemoveSlot}
+          handleChangeSlot={handleChangeSlot}
+        />
+      ))}
+    </form>
+  );
 }
 
 export function AvailabilityRow({
-	availability,
-	handleDisableDay,
+  availability,
+  handleDisableDay,
+  handleAddSlot,
+  handleRemoveSlot,
+  handleChangeSlot,
 }: {
-	availability: Day;
-	handleDisableDay: (day: string) => void;
+  availability: Day;
+  handleDisableDay: (day: string) => void;
+  handleAddSlot: (day: string) => void;
+  handleRemoveSlot: (day: string, index: number) => void;
+  handleChangeSlot: (day: string, index: number, slot: Slot) => void;
 }) {
-	return (
-		<div className="flex flex-col items-start gap-6 md:flex-row">
-			<div className="flex items-center w-40 gap-3">
-				<Switch.Root
-					id={`${availability.day}_is_available`}
-					className="SwitchRoot"
-					checked={availability.available}
-					onClick={() => handleDisableDay(availability.day)}
-				>
-					<Switch.Thumb className="SwitchThumb" />
-				</Switch.Root>
-				<label
-					htmlFor={`${availability.day}_is_available`}
-					className="flex items-center gap-4 text-md text-f-black"
-				>
-					{availability.day}
-				</label>
-			</div>
-			{availability.available && availability.slots.length > 0 && (
-				<div className="flex flex-col gap-1">
-					{availability.slots.map((slot, i) => (
-						<div key={slot.start} className="flex items-center gap-2">
-							<div className="w-28">
-								<Select
-									id={`${availability.day}_start_time`}
-									options={[{ value: slot.start, label: slot.start }]}
-									defaultValue={slot.start}
-									style="min-h-10"
-								/>
-							</div>
-							<div className="w-28">
-								<Select
-									id={`${availability.day}_end_time`}
-									options={[{ value: slot.end, label: slot.end }]}
-									defaultValue={slot.end}
-									style="min-h-10"
-								/>
-							</div>
-							{i === 0 && (
-								<button
-									aria-label="Agregar franja horaria"
-									type="button"
-									className="flex items-center justify-center w-8 h-8 transition duration-300 rounded-md hover:bg-neutral-200 focused-btn"
-									onClick={() => console.log("clicked")}
-								>
-									<PlusLg />
-								</button>
-							)}
-							{i > 0 && (
-								<button
-									aria-label="Eliminar franja horaria"
-									type="button"
-									className="flex items-center justify-center w-8 h-8 transition duration-300 rounded-md hover:bg-error-200 focused-btn"
-									onClick={() => console.log("clicked")}
-								>
-									<Trash3 />
-								</button>
-							)}
-						</div>
-					))}
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div className='flex flex-col items-start gap-6 md:flex-row'>
+      <div className='flex items-center w-40 gap-3'>
+        <Switch.Root
+          id={`${availability.day}_is_available`}
+          className='SwitchRoot'
+          checked={availability.available}
+          onClick={() => handleDisableDay(availability.day)}
+        >
+          <Switch.Thumb className='SwitchThumb' />
+        </Switch.Root>
+        <label
+          htmlFor={`${availability.day}_is_available`}
+          className='flex items-center gap-4 text-md text-f-black'
+        >
+          {availability.day}
+        </label>
+      </div>
+      {availability.available && availability.slots.length > 0 && (
+        <div className='flex flex-col gap-1'>
+          {availability.slots.map((slot, i) => (
+            <div key={slot.start} className='flex items-center gap-2'>
+              <div className='h-12 w-36'>
+                <SelectV2
+                  value={slot.start}
+                  options={START_TIMES}
+                  namespace={`${availability.day}_start_time`}
+                  onChange={(value) => {
+                    handleChangeSlot(availability.day, i, { ...slot, start: value });
+                  }}
+                  label='Selecciona hora de inicio'
+                />
+              </div>
+              <div className='h-12 w-36'>
+                <SelectV2
+                  value={slot.end}
+                  options={END_TIMES}
+                  namespace={`${availability.day}_end_time`}
+                  onChange={(value) => {
+                    handleChangeSlot(availability.day, i, { ...slot, end: value });
+                  }}
+                  label='Selecciona hora de fin'
+                />
+              </div>
+              {i === 0 && (
+                <button
+                  aria-label='Agregar franja horaria'
+                  type='button'
+                  className='flex items-center justify-center w-8 h-8 transition duration-300 rounded-md hover:bg-neutral-200 focused-btn'
+                  onClick={() => handleAddSlot(availability.day)}
+                >
+                  <PlusLg />
+                </button>
+              )}
+              {i > 0 && (
+                <button
+                  aria-label='Eliminar franja horaria'
+                  type='button'
+                  className='flex items-center justify-center w-8 h-8 transition duration-300 rounded-md hover:bg-error-200 focused-btn'
+                  onClick={() => handleRemoveSlot(availability.day, i)}
+                >
+                  <Trash3 />
+                </button>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
