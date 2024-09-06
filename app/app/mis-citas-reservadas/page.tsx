713c9d2ea7d0989377657e2255244bd8eaed28b6
Andres Argote <andresargote2019@gmail.com>
2024-09-06 17:08:43 +0200

    feat: 🎸 Added new page
    Agregados los componentes iniciales en la pagina de mis-citas-reservadas

---
+++ app/app/mis-citas-reservadas/page.tsx
@@ -0,0 +1,19 @@
+export default async function MisCitasReservadas() {
+  return (
+    <div className='px-4 py-6 mx-auto'>
+      <div className='container mx-auto lg:pl-72'>
+        <header className='flex flex-col justify-between w-full gap-6 mb-12 md:flex-row'>
+          <div>
+            <h1 className='mb-2 text-3xl font-semibold text-neutral-900'>
+              Mis Citas Reservadas
+            </h1>
+            <p className='font-light leading-relaxed text-neutral-800'>
+              Aquí puedes ver y gestionar todas tus citas reservadas.
+            </p>
+          </div>
+        </header>
+        <main></main>
+      </div>
+    </div>
+  );
+}
