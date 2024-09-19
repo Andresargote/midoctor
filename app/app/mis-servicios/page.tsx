3175de90c37870ee89db3d037908b7552daf21e3
Andres Argote <andresargote2019@gmail.com>
2024-09-19 10:59:59 +0200

    fix: 🐛 Fixed server side opening modal to client side
    Realizado fix para pasar de una apertura de los modelos por url a
    cliente, mejorando la velocidad de apertura, abstrayendo un componenten
    generico

--- app/app/mis-servicios/page.tsx
+++ app/app/mis-servicios/page.tsx
@@ -1,6 +1,8 @@
 import { createClient } from '@/app/lib/utils/supabase/server';
+import { GeneralWrapper } from '@/app/ui/components/GeneralWrapper';
+import { AddService } from '@/app/ui/components/Modals/AddService';
 import { ServicesList } from '@/app/ui/components/ServicesList';
-import Link from 'next/link';
+
 import { Suspense } from 'react';
 
 export default async function MisServicios() {
@@ -14,34 +16,16 @@ export default async function MisServicios() {
     .eq('user_id', data?.user?.id);
 
   return (
-    <div className='px-4 py-6 mx-auto'>
-      <div className='container mx-auto lg:pl-72'>
-        <header className='flex flex-col justify-between w-full gap-6 mb-12 md:flex-row'>
-          <div>
-            <h1 className='mb-2 text-3xl font-semibold text-neutral-900'>
-              Mis Servicios
-            </h1>
-            <p className='font-light leading-relaxed text-neutral-800'>
-              Aquí puedes ver y editar la información de tus servicios.
-            </p>
-          </div>
-          <div>
-            <Link
-              className='flex flex-col justify-center px-4 font-semibold transition duration-300 rounded-full w-fit bg-primary-500 hover:bg-primary-600 focused-btn min-h-14 text-f-white'
-              href='mis-servicios/?action=new'
-            >
-              Agregar servicio
-            </Link>
-          </div>
-        </header>
-
-        <Suspense fallback={<div>Cargando...</div>}>
-          <ServicesList
-            userId={data?.user?.id ?? ''}
-            servicesData={services.data ?? []}
-          />
-        </Suspense>
-      </div>
-    </div>
+    <Suspense fallback={<p>Cargando...</p>}>
+      <GeneralWrapper
+        userId={data?.user?.id ?? ''}
+        title='Mis Servicios'
+        description='Aquí puedes ver y editar la información de tus servicios.'
+        btnText='Agregar servicio'
+        data={services.data ?? []}
+        AddModal={AddService}
+        Content={ServicesList}
+      />
+    </Suspense>
   );
 }
--- app/ui/components/ConsultsList/index.tsx
+++ app/ui/components/ConsultsList/index.tsx
@@ -1,369 +1,166 @@
-"use client";
-import * as Dialog from "@radix-ui/react-dialog";
-import * as Switch from "@radix-ui/react-switch";
-import { X } from "react-bootstrap-icons";
-import PhoneInput from "react-phone-number-input";
-import { Button } from "../Button";
-import TextInput from "../TextInput";
-import "react-phone-number-input/style.css";
-import { addConsult as AddConsultAction } from "@/app/app/mi-consultorio/action";
-import type { Consult } from "@/app/lib/types";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
-import { Controller, useForm } from "react-hook-form";
-import { Toast } from "../Toast";
-import { consultSchema } from "./validate-schema";
-import Link from "next/link";
-import { deleteConsult } from "./action";
+'use client';
+import * as Dialog from '@radix-ui/react-dialog';
+import { Trash3, X } from 'react-bootstrap-icons';
+import { Button } from '../Button';
+import 'react-phone-number-input/style.css';
+import type { Consult } from '@/app/lib/types';
+import { useEffect, useState } from 'react';
+import { Toast } from '../Toast';
+import { deleteConsult } from './action';
 
-type ConsultFormDefaultValues = {
-	is_online: boolean;
-	name: string;
-	address: string;
-	phone_number: string;
-};
+export function ConsultsList({ data }: { data: Consult[] }) {
+  const [consults, setConsults] = useState<Consult[]>(data ?? []);
+  const [selectedConsult, setSelectedConsult] = useState<Consult | null>(null);
+  const [deleted, setDeleted] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-export function ConsultsList({
-	userId,
-	consultsData,
-}: {
-	userId: string;
-	consultsData: Consult[];
-}) {
-	const searchParams = useSearchParams();
-	const action = searchParams.get("action");
-	const router = useRouter();
+  const handleCloseDeleteModal = () => {
+    setIsOpen(false);
+    setSelectedConsult(null);
+  };
 
-	const [isOnline, setIsOnline] = useState(false);
-	const [consults, setConsults] = useState(consultsData ?? []);
-	const [added, setAdded] = useState(false);
-	const [error, setError] = useState<string | null>(null);
-	const [isLoading, setIsLoading] = useState(false);
+  const handleDeleteConsult = async (consultId: string) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      await deleteConsult(consultId);
 
-	const {
-		setValue,
-		register,
-		handleSubmit,
-		reset,
-		formState: { errors },
-		control,
-	} = useForm<ConsultFormDefaultValues>({
-		defaultValues: {
-			is_online: false,
-			name: "",
-			address: "",
-			phone_number: "",
-		},
-		resolver: zodResolver(consultSchema(isOnline)),
-	});
+      setConsults((prev) => prev.filter((c) => c.id !== consultId));
+      setDeleted('Consultorio eliminado exitosamente');
+    } catch (e) {
+      setError(
+        'Ocurrió un error al intentar eliminar el consultorio, por favor intenta de nuevo, si el problema persiste contacta con soporte.'
+      );
+    } finally {
+      setIsLoading(false);
+      handleCloseDeleteModal();
+    }
+  };
 
-	const onSubmit = async (formValues: ConsultFormDefaultValues) => {
-		setIsLoading(true);
-		setError(null);
-		try {
-			const { data, error } = await AddConsultAction({
-				...formValues,
-				user_id: userId,
-			});
+  useEffect(() => {
+    setConsults(data);
+  }, [data]);
 
-			if (error) {
-				throw new Error();
-			}
+  return (
+    <>
+      <Dialog.Root open={isOpen}>
+        <Dialog.Portal>
+          <Dialog.Overlay className='fixed top-0 left-0 z-50 w-full h-full bg-f-black opacity-15' />
 
-			setAdded(true);
+          <Dialog.Content
+            aria-disabled={isLoading}
+            onInteractOutside={() => {
+              if (!isLoading) {
+                handleCloseDeleteModal();
+              }
+            }}
+            onEscapeKeyDown={() => {
+              if (!isLoading) {
+                handleCloseDeleteModal();
+              }
+            }}
+            className='fixed z-50 w-full max-w-md p-8 transform shadow-sm bg-f-white rounded-2xl top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4'
+            style={{
+              transform: 'translate(-50%, -50%)',
+              width: 'calc(100% - 1rem)',
+            }}
+            onOpenAutoFocus={(event) => {
+              event.preventDefault();
+            }}
+          >
+            <Dialog.Title className='max-w-[90%] mb-2 text-2xl font-semibold leading-relaxed text-neutral-900'>
+              ¿Eliminar consultorio?
+            </Dialog.Title>
+            <Dialog.Description className='mb-6 text-sm font-light leading-relaxed text-neutral-800'>
+              <strong>¡Esta acción no tiene vuelta atrás!</strong>
+              <br />
+              ¿Seguro que quieres eliminar el consultorio{' '}
+              <strong className='font-bold'>{selectedConsult?.name}</strong>?
+            </Dialog.Description>
 
-			if (data) {
-				setConsults((prevConsults) => [...prevConsults, data]);
-			}
+            <div className='flex items-center justify-end gap-3'>
+              <Dialog.Close asChild>
+                <Button
+                  bgColorKey='neutral'
+                  type='button'
+                  disabled={isLoading}
+                  onClick={() => {
+                    if (!isLoading) {
+                      handleCloseDeleteModal();
+                    }
+                  }}
+                >
+                  Cancelar
+                </Button>
+              </Dialog.Close>
+              <Button
+                bgColorKey='error'
+                type='button'
+                isLoading={isLoading}
+                disabled={isLoading}
+                onMouseDown={() => handleDeleteConsult(selectedConsult?.id ?? '')}
+              >
+                Eliminar
+              </Button>
+            </div>
+            <button
+              onClick={() => {
+                if (!isLoading) {
+                  handleCloseDeleteModal();
+                }
+              }}
+              type='button'
+              className='absolute p-2 rounded-full focused-btn top-4 right-4 bg-neutral-100'
+              aria-label='Cerrar modal de eliminar consultorio'
+            >
+              <X color='#0A0A0A' width={24} height={24} />
+            </button>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+      {error && <Toast type='error' message={error} />}
+      {deleted && <Toast type='success' message={deleted} />}
+      {consults.length === 0 ? (
+        <p className='text-lg font-light text-center text-neutral-800'>
+          Aún no tienes consultorios registrados
+        </p>
+      ) : (
+        <main className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 auto-rows-fr'>
+          {consults?.map((consult) => (
+            <div key={consult.id} className='h-full p-4 rounded-lg shadow-sm bg-f-white'>
+              <div className='flex justify-between'>
+                <h2 className='mb-2 text-xl font-semibold text-neutral-900'>
+                  {consult.name}
+                </h2>
 
-			handleResetFormValues();
-		} catch (error) {
-			setError(
-				"Ocurrió un error al agregar el consultorio, por favor intenta de nuevo, si el problema persiste contacta con soporte.",
-			);
-		} finally {
-			setIsLoading(false);
-		}
-	};
-
-	const handleResetFormValues = () => {
-		reset({
-			is_online: false,
-			name: "",
-			address: "",
-			phone_number: "",
-		});
-		setIsOnline(false);
-		router.back();
-	};
-
-	const handleCloseDeleteModal = () => {
-		router.back();
-	};
-
-	const handleDeleteConsult = async (consultId: string) => {
-		setIsLoading(true);
-		setError(null);
-		try {
-			await deleteConsult(consultId);
-
-			setConsults((prev) => prev.filter((c) => c.id !== consultId));
-		} catch (e) {
-			setError("Ha ocurrido un error al eliminar el consultorio");
-		} finally {
-			setIsLoading(false);
-			router.back();
-		}
-	};
-
-	return (
-		<>
-			<Dialog.Root open={action === "new"}>
-				<Dialog.Portal>
-					<Dialog.Overlay className="fixed top-0 left-0 z-50 w-full h-full bg-f-black opacity-15" />
-
-					<Dialog.Content
-						aria-disabled={isLoading}
-						onInteractOutside={() => {
-							if (!isLoading) {
-								handleResetFormValues();
-							}
-						}}
-						onEscapeKeyDown={() => {
-							if (!isLoading) {
-								handleResetFormValues();
-							}
-						}}
-						className="fixed z-50 w-full max-w-md p-8 transform shadow-sm bg-f-white rounded-2xl top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4"
-						style={{
-							transform: "translate(-50%, -50%)",
-							width: "calc(100% - 1rem)",
-						}}
-						onOpenAutoFocus={(event) => {
-							event.preventDefault();
-						}}
-					>
-						<Dialog.Title className="max-w-[90%] mb-2 text-2xl font-semibold leading-relaxed text-neutral-900">
-							Agregar nuevo consultorio
-						</Dialog.Title>
-						<Dialog.Description className="mb-6 text-sm font-light leading-relaxed text-neutral-800">
-							Por favor, proporciona la información necesaria para agregar un
-							nuevo consultorio.
-						</Dialog.Description>
-
-						<form
-							className="flex flex-col gap-6"
-							onSubmit={handleSubmit(onSubmit)}
-						>
-							<div className="flex items-center gap-3">
-								<Switch.Root
-									id="is_online"
-									className="SwitchRoot"
-									checked={isOnline}
-									onCheckedChange={(checked) => {
-										setIsOnline(checked);
-										setValue("is_online", checked);
-									}}
-									{...register("is_online")}
-								>
-									<Switch.Thumb className="SwitchThumb" />
-								</Switch.Root>
-								<label
-									htmlFor="is_online"
-									className="flex items-center gap-4 text-sm text-f-black"
-								>
-									Consulta virtual
-								</label>
-							</div>
-							<TextInput
-								label="Nombre del consultorio"
-								id="name"
-								{...register("name")}
-								errorMessage={(errors.name?.message as string) ?? ""}
-								autoFocus
-								helperText="El nombre de tu consultorio solo es visible para ti."
-							/>
-							{!isOnline && (
-								<TextInput
-									label="Dirección del consultorio"
-									id="address"
-									{...register("address")}
-									errorMessage={(errors.address?.message as string) ?? ""}
-								/>
-							)}
-
-							<div className="flex flex-col gap-1.5">
-								<label
-									className="text-sm font-light text-neutral-600"
-									htmlFor="phone_number"
-								>
-									Teléfono del consultorio
-								</label>
-								<Controller
-									name="phone_number"
-									control={control}
-									render={({ field }) => (
-										<PhoneInput
-											id="phone_number"
-											placeholder="Número de teléfono"
-											defaultCountry="VE"
-											countries={["VE"]}
-											country="VE"
-											value={field.value}
-											onChange={(value) => {
-												field.onChange(value);
-												setValue("phone_number", value as string);
-											}}
-										/>
-									)}
-								/>
-								{errors.phone_number && (
-									<span className="text-xs font-semibold text-error-500 font-xs">
-										{errors.phone_number.message}
-									</span>
-								)}
-							</div>
-							<div className="flex items-center justify-end gap-3">
-								<Dialog.Close asChild>
-									<Button
-										bgColorKey="neutral"
-										type="button"
-										disabled={isLoading}
-										onClick={() => {
-											handleResetFormValues();
-										}}
-									>
-										Cancelar
-									</Button>
-								</Dialog.Close>
-
-								<Button
-									bgColorKey="success"
-									type="submit"
-									isLoading={isLoading}
-									disabled={isLoading}
-								>
-									Guardar
-								</Button>
-							</div>
-						</form>
-
-						<Dialog.Close asChild>
-							<button
-								onClick={() => {
-									handleResetFormValues();
-								}}
-								type="button"
-								className="absolute p-2 rounded-full focused-btn top-4 right-4 bg-neutral-100"
-								aria-label="Cerrar modal de creación de consultorio"
-							>
-								<X color="#0A0A0A" width={24} height={24} />
-							</button>
-						</Dialog.Close>
-					</Dialog.Content>
-				</Dialog.Portal>
-			</Dialog.Root>
-			<Dialog.Root open={action === "delete"}>
-				<Dialog.Portal>
-					<Dialog.Overlay className="fixed top-0 left-0 z-50 w-full h-full bg-f-black opacity-15" />
-
-					<Dialog.Content
-						aria-disabled={isLoading}
-						onInteractOutside={() => { }}
-						onEscapeKeyDown={() => { }}
-						className="fixed z-50 w-full max-w-md p-8 transform shadow-sm bg-f-white rounded-2xl top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4"
-						style={{
-							transform: "translate(-50%, -50%)",
-							width: "calc(100% - 1rem)",
-						}}
-						onOpenAutoFocus={(event) => { }}
-					>
-						<Dialog.Title className="max-w-[90%] mb-2 text-2xl font-semibold leading-relaxed text-neutral-900">
-							Eliminar consultorio
-						</Dialog.Title>
-						<Dialog.Description className="mb-6 text-sm font-light leading-relaxed text-neutral-800">
-							<strong>Esta acción no puede deshacerse!</strong>
-							<br />
-							Quieres eliminar el consultorio "
-							{searchParams.get("consult_name")}".
-						</Dialog.Description>
-
-						<div className="flex items-center justify-end gap-3">
-							<Dialog.Close asChild>
-								<Button
-									bgColorKey="neutral"
-									type="button"
-									disabled={isLoading}
-									onClick={handleCloseDeleteModal}
-								>
-									Cancelar
-								</Button>
-							</Dialog.Close>
-							<Button
-								bgColorKey="error"
-								type="button"
-								isLoading={isLoading}
-								disabled={isLoading}
-								onMouseDown={() =>
-									handleDeleteConsult(searchParams.get("consult_id") as string)
-								}
-							>
-								Eliminar
-							</Button>
-						</div>
-						<button
-							onClick={handleCloseDeleteModal}
-							type="button"
-							className="absolute p-2 rounded-full focused-btn top-4 right-4 bg-neutral-100"
-							aria-label="Cerrar modal de eliminar consultorio"
-						>
-							<X color="#0A0A0A" width={24} height={24} />
-						</button>
-					</Dialog.Content>
-				</Dialog.Portal>
-			</Dialog.Root>
-			{error && <Toast type="error" message={error} />}
-			{added && (
-				<Toast type="success" message="Consultorio agregado exitosamente" />
-			)}
-			{consults.length === 0 ? (
-				<p className="text-lg font-light text-center text-neutral-800">
-					Aún no tienes consultorios registrados
-				</p>
-			) : (
-				<main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 auto-rows-fr">
-					{consults?.map((consult) => (
-						<div
-							key={consult.id}
-							className="h-full p-4 rounded-lg shadow-sm bg-f-white"
-						>
-							<div className="flex justify-between">
-								<h2 className="mb-2 text-xl font-semibold text-neutral-900">
-									{consult.name}
-								</h2>
-								<Link
-									href={`mi-consultorio/?action=delete&consult_id=${consult.id}&consult_name=${consult.name}`}
-								>
-									<X />
-								</Link>
-							</div>
-							<div className="p-2 rounded-full w-fit bg-neutral-100">
-								<p className="text-xs font-base text-neutral-900">
-									{consult.is_online
-										? "Consulta virtual"
-										: "Consulta presencial"}
-								</p>
-							</div>
-							{consult.address && (
-								<p className="mt-4 text-sm font-light leading-relaxed text-neutral-800">
-									{consult.address}
-								</p>
-							)}
-						</div>
-					))}
-				</main>
-			)}
-		</>
-	);
+                <button
+                  onClick={() => {
+                    setSelectedConsult(consult);
+                    setIsOpen(true);
+                  }}
+                  type='button'
+                  className='flex items-center justify-center w-8 h-8 transition duration-300 rounded-md hover:bg-error-50 focused-btn'
+                  aria-label='Botón para eliminar consultorio'
+                >
+                  <Trash3 color='#EF4444' />
+                </button>
+              </div>
+              <div className='p-2 rounded-full w-fit bg-neutral-100'>
+                <p className='text-xs font-base text-neutral-900'>
+                  {consult.is_online ? 'Consulta virtual' : 'Consulta presencial'}
+                </p>
+              </div>
+              {consult.address && (
+                <p className='mt-4 text-sm font-light leading-relaxed text-neutral-800'>
+                  {consult.address}
+                </p>
+              )}
+            </div>
+          ))}
+        </main>
+      )}
+    </>
+  );
 }
