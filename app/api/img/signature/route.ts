53b16ace97afcc968c875813f00a9005cc4b1cd5
Andres Argote <andresargote2019@gmail.com>
2024-11-20 19:10:12 +0100

    Fixed accurate timestamp error

--- app/api/img/signature/route.ts
+++ app/api/img/signature/route.ts
@@ -1,9 +1,20 @@
 import { v2 as cloudinary } from 'cloudinary';
 
-export async function GET() {
-	const timestamp = Math.round(Date.now() / 1000);
+async function getAccurateTimestamp() {
+	try {
+		const response = await fetch('https://worldtimeapi.org/api/ip');
+		const data = await response.json();
+		return Math.round(new Date(data.utc_datetime).getTime() / 1000);
+	} catch (error) {
+		console.error('Error fetching time:', error);
+		return Math.round(Date.now() / 1000);
+	}
+}
 
+export async function GET() {
+	const timestamp = await getAccurateTimestamp();
 	const apiSecret = process.env.CLOUDINARY_API_SECRET ?? '';
+
 	const signature = cloudinary.utils.api_sign_request(
 		{
 			timestamp: timestamp,
